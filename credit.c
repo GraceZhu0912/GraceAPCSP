1f3f62cfeacbf1fb15eb1ba99ed8cba38544709a
GraceZhu0912 <GraceZhu0912@users.noreply.github.com>
2023-09-27 22:17:37 -0700

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -9,13 +9,15 @@
 
 int main(void)
 {
-    long number = get_long("Number: ");
-    int length = floor(1+log10(number));
+    long original = get_long("Number: ");
+    int length = floor(1+log10(original));
+    //printf("length = %i\n", length);
     int sum = 0;
     int counter = 0;
     int digit;
     //printf("number = %ld and length = %i\n",number,length);
-
+    long number = original;
+    //printf("o %ld ",original);
     while (number > 0)
     {
         //digit =(number / (int)(pow(10,counter))) % (int)(pow(10,counter + 1));
@@ -24,12 +26,12 @@ int main(void)
         number = number / 10;
         counter += 1;
         //printf("sum = %i, digit = %i, number = %ld, counter = %i\n",sum, digit, number, counter);
-        
+
         //digit = (number / (int)(pow(10,counter))) % (int)(pow(10,counter + 1));
         digit = (number % 10) * 2;
         if(digit>9)
         {
-            digit = digit / 10 + digit %10;
+            digit = (digit / 10) + (digit %10);
         }
         sum = sum + digit;
         number = number / 10;
@@ -41,9 +43,11 @@ int main(void)
     //printf("sum = %i",sum);
     if (sum % 10 == 0)
     {
-        int first_digit = number/(int)(pow(10,length-1));
-        int second_digit = number/(int)(pow(10,length-2)) % 10;
-        //printf("first digit = %i, second digit = %i",first_digit,second_digit);
+        //printf("or = %ld",original);
+        long first_digit = original/(long)(pow(10,length-1));
+        long second_digit = original/(long)(pow(10,length-2)) % 10;
+
+        //printf("first digit = %ld, second digit = %ld",first_digit,second_digit);
 
         if (length == 15)
         {
