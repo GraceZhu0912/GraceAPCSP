89e4a1f21cfb8dd523f34caaca228e21d3fc680f
GraceZhu0912 <GraceZhu0912@users.noreply.github.com>
2023-10-20 11:17:33 -0700

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -31,16 +31,16 @@ int main(void)
 
     if (index < 1)
     {
-        printf("Before Grade 1");
+        printf("Before Grade 1\n");
     }
     else if (index > 16)
     {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
     else
     {
         index = round(index);
-        printf("Grade %i",(int)index);
+        printf("Grade %i\n",(int)index);
     }
 }
 
