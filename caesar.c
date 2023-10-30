a3754a7b881c2dfcbd43dde9d11307de0a9bba5f
GraceZhu0912 <GraceZhu0912@users.noreply.github.com>
2023-10-30 10:20:56 -0700

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -19,32 +19,32 @@ int main(int argc, string argv[])
                 return 1;
             }
     }
-
     string message = get_string("plaintext: ");
     int key = atoi(argv[1]);
-    key = key%26;
     int i;
     char ascii;
 
     for (i = 0; i<strlen(message); i++)
     {
         ascii = message[i];
+
         if (isalpha(message[i]))
         {
-            ascii += key;   
-
             if (islower(message[i]))
             {
-                    ascii = ((ascii - 97) % 26) + 97;
+                    ascii = (ascii + key - 97) % 26 + 97;
+                    printf("lower %c",ascii);
 
             }
             else if (isupper(message[i]))
             {
-                    ascii = ((ascii - 65) % 26) + 65;
+                    ascii = (ascii + key - 65) % 26 + 65;
+                    printf("upper %c",ascii);
             }
         }
 
         message[i] = ascii;
     }
+    printf("\n");
     printf("ciphertext: %s\n", message);
 }
