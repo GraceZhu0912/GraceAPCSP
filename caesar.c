3474ecfedfcedb51205e95e0ff4a2f56fa575451
GraceZhu0912 <GraceZhu0912@users.noreply.github.com>
2023-10-30 10:23:21 -0700

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main(int argc, string argv[])
 {
     if (argc != 2)
@@ -12,19 +12,19 @@ int main(int argc, string argv[])
     }
 
     int j;
-    for (j=0; j < strlen(argv[1]); j++)
+    for (j = 0; j < strlen(argv[1]); j++)
     {
         if ((argv[1][j] < 48) || (argv[1][j] > 57))
-            {
-                return 1;
-            }
+        {
+            return 1;
+        }
     }
     string message = get_string("plaintext: ");
     int key = atoi(argv[1]);
     int i;
     char ascii;
 
-    for (i = 0; i<strlen(message); i++)
+    for (i = 0; i < strlen(message); i++)
     {
         ascii = message[i];
 
@@ -32,14 +32,13 @@ int main(int argc, string argv[])
         {
             if (islower(message[i]))
             {
-                    ascii = (ascii + key - 97) % 26 + 97;
-                    printf("lower %c",ascii);
-
+                ascii = (ascii + key - 97) % 26 + 97;
+                printf("lower %c", ascii);
             }
             else if (isupper(message[i]))
             {
-                    ascii = (ascii + key - 65) % 26 + 65;
-                    printf("upper %c",ascii);
+                ascii = (ascii + key - 65) % 26 + 65;
+                printf("upper %c", ascii);
             }
         }
 
