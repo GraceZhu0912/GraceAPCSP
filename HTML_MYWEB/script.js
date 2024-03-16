1ad494df45a3528dd0fc608d514a9d2d54ddb595
GraceZhu0912 <142924190+GraceZhu0912@users.noreply.github.com>
2024-03-16 00:04:09 -0700

    Add files via upload

---
+++ HTML_MYWEB/script.js
@@ -0,0 +1,15 @@
+let counter = localStorage.getItem('counter') || 0;
+
+// Function to increment the counter and update the display
+function incrementCounter() {
+    counter++;
+    // Update the display
+    document.getElementById('counterDisplay').innerText = counter;
+    // Store the updated counter value in localStorage
+    localStorage.setItem('counter', counter);
+}
+
+// Function to initialize the counter display when the page loads
+window.onload = function() {
+    document.getElementById('counterDisplay').innerText = counter;
+};
